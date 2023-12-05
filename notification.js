ecc5da81798ef5b263dc99467a4943a3b123245f
Ben Cunningham <bmcunningham4@outlook.com>
2023-12-05 15:10:34 +0800

    Finalised website with included description!

--- notification.js
+++
@@ -1,7 +0,0 @@
-function scheduleNotification() {
-    setTimeout(function() {
-      alert("Time's up! This is your notification."); // Display an alert as a pop-up
-      // You can also use browser notifications: Notification.requestPermission().then(() => new Notification("Title", { body: "Message" }));
-    }, 5000); // Set time in milliseconds (e.g., 5000 for 5 seconds)
-  }
-  
